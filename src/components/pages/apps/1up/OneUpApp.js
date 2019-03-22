d7ec8ee735a328e77892d61965bbbc0f2355ac92
Jordan Denison <jordan@denisonweb.com>
2019-03-22 18:04:55 -0500

    remove timeout

--- src/components/pages/apps/1up/OneUpApp.js
+++ src/components/pages/apps/1up/OneUpApp.js
@@ -5,8 +5,6 @@ import superagent from 'superagent'
 import { url } from 'lib/feathers/feathersClient'
 import OneUpAppView from './OneUpAppView'
 
-const twentyMinutes = 1200000
-
 const mapStateToProps = state => ({
   currentUser: state.currentUser,
   processing: state.http.processing,
@@ -20,7 +18,6 @@ const mapDispatchToProps = dispatch => {
       try {
         dispatch({ type: 'HTTP_REQUEST_START' })
         await superagent
-          .timeout(twentyMinutes)
           .post(`${url}/sync-1up-data`)
           .set('Authorization', `Bearer ${window.localStorage && window.localStorage.getItem && window.localStorage.getItem('feathers-jwt')}`)
         dispatch({ type: 'HTTP_REQUEST_SUCCESS' })
